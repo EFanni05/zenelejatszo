870208d00e9ba653e2cf7e897698e4f14f2f060f
Fannichan <elekes.fanni2005@gmail.com>
2023-12-12 13:07:38 +0100

    connection create

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,10 @@ import * as mysql from 'mysql2';
 import { AppService } from './app.service';
 
 const conn = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'database',
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'music_player'
 }).promise();
 
 @Controller()
